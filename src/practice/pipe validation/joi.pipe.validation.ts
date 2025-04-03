c20ab9014053e8fb926526e64deb4f2801800323
ubaid2917 <ubaid29170@gmail.com>
2025-04-03 20:47:31 +0500

    add joi validation

--- src/practice/pipe validation/joi.pipe.validation.ts
+++ src/practice/pipe validation/joi.pipe.validation.ts
@@ -6,13 +6,13 @@ export class JoiValidationPipe implements PipeTransform {
     constructor(private schema: ObjectSchema) {}
 
     transform(value: Record<string, any>) {
-        const { error, value: validatedValue } = this.schema.validate(value, { abortEarly: false });
+        const { error, value: validatedValue } = this.schema.validate(value, { convert: false });  // ✅ Prevent auto conversion
 
         if (error) {
-            console.log('Validation Errors:', error.details.map(err => err.message));  // ✅ Log the actual error
-            throw new BadRequestException(error.details.map(err => err.message));
+         
+            throw new BadRequestException(error.details.map(err => err.message.replace(/\"/g, "")));   // actual logs
         }
 
-        return validatedValue;  // ✅ Return validated data
+        return validatedValue;  // ✅ Return validated data if no errors
     }
 }
